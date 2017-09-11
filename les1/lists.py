3639f8a8b6f5cf3dc6cadde7fd8c3ebae371c766
Apsalamov Sultan <sapsalamov@gmail.com>
2017-09-11 21:11:36 +0600

    Добавляет примеры работы с основными типами данных в python.

---
+++ les1/lists.py
@@ -0,0 +1,7 @@
+mylist = [1, 'два', 3.1]
+print(len(mylist)) # Число элементов в списке
+print(mylist[0]) # Дооступ к первому элементу
+mylist.append("new element") 
+# Добавляет новый элемент в конец списка
+print("Добавлен----> ", mylist)
+mylist.pop(0)  
---
+++ les1/strings.py
@@ -0,0 +1,11 @@
+line = "Это строка"
+print(line)
+print("Длина строки: ", len(line))
+print(line[0]) # первый элемент, начинается с нуля
+print(line[1]) # Второй элемент
+print(line[-1]) # Последний элемент
+print(line[1:4]) # Срез
+print(line[:4]) # 
+print(line[4:]) #
+print(line + line) # Сложение строк
+print(line * 3) # Повторение строки
