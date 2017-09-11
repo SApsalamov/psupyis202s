5c410f48be9d629cbf534ab7bda7feb3b3e41d7b
Apsalamov Sultan <sapsalamov@gmail.com>
2017-09-12 00:39:46 +0600

    Изменяет файл lists.py. Добавлен пример со вложенными списками.

--- les1/lists.py
+++ les1/lists.py
@@ -4,4 +4,9 @@ print(mylist[0]) # Дооступ к первому элементу
 mylist.append("new element") 
 # Добавляет новый элемент в конец списка
 print("Добавлен----> ", mylist)
-mylist.pop(0)  
+mylist.pop(0)  # Удаляем первый элемент
+print("Удалён---->", mylist)
+# Вложенные списки
+superlist = [[1, 2, 3], [4, 5, 6]]
+print(superlist)
+print(superlist[1][1]) # Удаляем второй элемент второго списка
