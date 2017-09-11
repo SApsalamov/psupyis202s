8f0bcb3cb5523aee90961005e877f02b237f6a71
Apsalamov Sultan <sapsalamov@gmail.com>
2017-09-12 01:09:45 +0600

    Добавляет пример работы со словарями.

---
+++ les1/dicts.py
@@ -0,0 +1,5 @@
+mydic = {1: "Первый", 2: "Второй"}
+print("Возьмём элемент с ключом 1 ---->", mydic[1])
+superdic = {'name': {'first': 'Александр', 'second': 'Иванов'},
+			'job': ['ОИТ', 'ОТК']}
+print(superdic['name']['second'], "--->", superdic['job'][0])
