09e885250c2e41a3df327684c469b4cc85898345
Apsalamov Sultan <sapsalamov@gmail.com>
2017-10-16 00:15:35 +0600

    Изменяет 10-ю практическую работу. Берёт срез из списка big_list и присваевает его одноименной пременной. Прибавляет 2 элемента к списку и отображает его.

--- lections/pr_l10_lists.py
+++ lections/pr_l10_lists.py
@@ -3,8 +3,13 @@ list2 = [11, 21, 22, 23, 24]
 print("list1 = ", list1)
 print("list2 = ", list2)
 print(list1[1])
-list2[-2] = 70
+list2[-1] = 70
 print(list2)
 big_list = list1 + list2
 print(big_list)
 
+srez_big_list = big_list[3:-2]
+print(srez_big_list)
+srez_big_list.append(90)
+srez_big_list.append(30)
+print(srez_big_list)
